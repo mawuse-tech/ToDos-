a1afc2e086435ba065d35c47bb344ac4265ec9e1
mawuse-tech <akpekemawuse1@gmail.com>
2025-05-07 13:17:47 +0000

    added the addTask function

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-const addTaskBtn = document.getElementById('addTaskBtn');
-const taskInput = document.getElementById('taskInput');
-const taskList = document.getElementById('taskList')
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+
+addTaskBtn.addEventListener("click", addTask);
+
+function addTask() {
+  const taskText = taskInput.value.trim();
+  if (taskText === "") return;
+
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.textContent = taskText;
+
+  span.addEventListener("click", () => {
+    li.classList.toggle("completed");
+  });
+
+
+}
