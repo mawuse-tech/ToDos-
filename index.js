24798e72046e3611d46887ef00f5ae772ba375a8
mawuse-tech <akpekemawuse1@gmail.com>
2025-05-10 17:51:09 +0000

    added text to the local storage

--- index.js
+++ index.js
@@ -2,32 +2,45 @@ const addTaskBtn = document.getElementById("addTaskBtn");
 const taskInput = document.getElementById("taskInput");
 const taskList = document.getElementById("taskList");
 
+let tasks = [];
+window.addEventListener("DOMContentLoaded", () => {
+  savedTasks = localStorage.getItem('todos');
+  if (savedTasks) {
+    tasks = JSON.parse(savedTasks);
+    tasks.forEach(task => {
+      renderTask(task)
+    });
+
+  }
+})
+
 addTaskBtn.addEventListener("click", addTask);
 
 function addTask() {
   const taskText = taskInput.value.trim();
-  if (taskText === "") return;
+  if (!taskText) return;
 
+  tasks.push(taskText);
+  localStorage.setItem("todos", JSON.stringify(tasks));
+  renderTask(taskText);
+  taskInput.value = ""
+};
+
+function renderTask(taskText) {
   const li = document.createElement("li");
+
   const span = document.createElement("span");
   span.textContent = taskText;
 
-  span.addEventListener("click", () => {
-    li.classList.toggle("completed");
-  });
-
   const deleteBtn = document.createElement("button");
-  const icon = document.createElement("i");
-  icon.classList.add("fas", "fa-trash");
-  deleteBtn.appendChild(icon);
-  deleteBtn.classList.add("delete-btn");
+  deleteBtn.textContent = "🗑";
   deleteBtn.addEventListener("click", () => {
     taskList.removeChild(li);
+    tasks = tasks.filter(task => task !== taskText);
+    localStorage.setItem("todos", JSON.stringify(tasks));
   });
 
   li.appendChild(span);
   li.appendChild(deleteBtn);
   taskList.appendChild(li);
-
-  taskInput.value = "";
 }
